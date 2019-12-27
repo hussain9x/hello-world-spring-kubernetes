57c0e46089b0f2f32e851a4d007266f232b47e7d
Hussain Al-Sulaiman <hussain9x@gmail.com>
2019-12-27 10:06:30 +0300

    first commit

---
+++ src/main/java/com/hussain9x/helloworldspringkubernetes/HelloWorldSpringKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.hussain9x.helloworldspringkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hussain9x/helloworldspringkubernetes/HelloWorldSpringKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussain9x.helloworldspringkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
